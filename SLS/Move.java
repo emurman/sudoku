1b3f9e064ca086ea6b5581f481dffd47718cc68e
Emil Murman <emilmurman@hotmail.com>
2020-02-28 16:02:42 +0100

    make fields final, implement toString

--- SLS/Move.java
+++ SLS/Move.java
@@ -1,10 +1,10 @@
 
 
 public class Move {
-    int x;
-    int y;
-    int old;
-    int new_;
+    final int x;
+    final int y;
+    final int old;
+    final int new_;
 
     public Move(int x, int y, int old, int new_) {
         this.x = x;
@@ -13,4 +13,8 @@ public class Move {
         this.new_ = new_;
     }
 
+    @Override
+    public String toString() {
+        return this.old + "@(" + this.x + ", " + this.y + ")" +" -> " + this.new_;
+    }
 }
